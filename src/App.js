b4460ea5f63c31641959ea71202bb7448e47ba49
Unnati Gandhi <95961486+unnati1004@users.noreply.github.com>
2022-04-17 03:12:42 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+import {AllRoutes} from "./Routes/routes"
+function App() {
+  return (
+    <div className="App">
+      <AllRoutes/>
+    </div>
+  );
+}
+export default App;
